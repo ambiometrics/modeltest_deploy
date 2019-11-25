bc27fb0ae890aefb8beb4001997696a28d34f083
Edwin Rodríguez <edwrodrig@gmail.com>
2019-11-25 18:29:06 -0300

    Modeltest unavailable

--- 01.prepare.sh
+++ 01.prepare.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 #This instructions are based on the README at following location https://github.com/ddarriba/modeltest
-
+#info about windows
+#https://github.com/ddarriba/modeltest/wiki/Download-and-Install
 #download the code creating a sources directory
+#instalar msys2 no funciono
+#cmake instalar y usar -D para definir variables
+#igual no funciona
+
 git clone --recursive https://github.com/ddarriba/modeltest.git sources
 
 #compile the PTHREAD version, there are a MPI version but this does not make any sense for bosque because is machine independent
