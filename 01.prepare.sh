9f462d051569b057df0ab8f665524c90f20e2833
Edwin Rodríguez <edwrodrig@gmail.com>
2019-11-27 14:32:15 -0300

    Advances in windows compilation but not compiled yet

--- 01.prepare.sh
+++ 01.prepare.sh
@@ -7,12 +7,18 @@
 #cmake instalar y usar -D para definir variables
 #igual no funciona
 
+rm sources -rf
 git clone --recursive https://github.com/ddarriba/modeltest.git sources
 
 #compile the PTHREAD version, there are a MPI version but this does not make any sense for bosque because is machine independent
 cd sources
 mkdir build && cd build
-cmake ..
+cmake \
+-DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+-DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
+-DCMAKE_SYSTEM_NAME=Windows \
+-DCMAKE_FIND_ROOT_PATH=/usr/x86_64-w64-mingw32 \
+-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY ..
 make
 
 #The executable is in sources/bin/modeltest-ng
@@ -21,3 +27,4 @@ cd ../..
 rm -rf release
 mkdir release -p
 cp sources/bin/modeltest-ng release
+touch release/modeltest.unavailable
